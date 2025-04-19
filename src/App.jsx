89eca4376a21596fb04fd4a2942f8a77f3d530ef
Mariana Piovezan <piovmari@gmail.com>
2025-04-19 19:22:28 -0300

    feat: add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button"
+
 function App() {
 
   return (
+    <>
    <h1 className="text-brand-red">Solta Move</h1>
+   <Button title={"Experimente por 15 dias"} link="ssdfd" />
+   </>
   )
 }
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = ({ link = "", title, primary = false }) => {
+  return (
+    <a href={link} className={`text-xs md:text-lg font-extrabold rounded-full py-2 px-4 border-2 border-brand-dark ${primary ? "text-brand-dark bg-brand-yellow" : "text-brand-light bg-brand-blue-dark"} hover:brightness-105 duration-300 ease-in-out `}>
+      {title}
+    </a>
+  );
+};
