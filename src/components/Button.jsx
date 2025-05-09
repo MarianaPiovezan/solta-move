f44f7332388dcdc55d560684f1447c396614b8d4
Mariana Piovezan <piovmari@gmail.com>
2025-05-09 16:19:59 -0300

    feat: adjusted scroll margin top button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,29 @@
-import iconWhatsapp from "/assets/images/icons/whatsapp.svg"
+import iconWhatsapp from "/assets/images/icons/whatsapp.svg";
 
-export const Button = ({ link = "", title, primary = true, className ='', icon=false, target }) => {
-  return (
-    <a href={link} className={` md:text-lg font-extrabold flex items-center justify-center gap-4 rounded-full py-3 px-4 md:px-8 border-2 border-brand-dark text-center ${primary ? "text-brand-dark bg-brand-yellow" : "text-brand-light bg-brand-blue-dark"} hover:brightness-125 duration-300 ease-in-out leading-none ${className}`} target={target}>
-      { icon && (
-        <img src={iconWhatsapp} alt="Whatsapp" loading="lazy" className="w-4 md:w-6 "/>
-      )}
+export const Button = ({ link = "", title, primary = true, className = '', icon = false, target }) => {
+  const handleClick = (e) => {
+    if (link.startsWith("#")) {
+      e.preventDefault(); 
+      const id = link.replace("#", "");
+      const targetElement = document.getElementById(id);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
+ 
+  const isInternalAnchor = link.startsWith("#");
+
+  const commonClasses = `md:text-lg font-extrabold flex items-center justify-center gap-4 rounded-full py-3 px-4 md:px-8 border-2 border-brand-dark text-center ${primary ? "text-brand-dark bg-brand-yellow" : "text-brand-light bg-brand-blue-dark"} hover:brightness-125 duration-300 ease-in-out leading-none ${className}`;
+
+  return isInternalAnchor ? (
+    <button onClick={handleClick} className={commonClasses}>
+      {title}
+    </button>
+  ) : (
+    <a href={link} target={target} className={commonClasses}>
+      {icon && <img src={iconWhatsapp} alt="Whatsapp" loading="lazy" className="w-4 md:w-6" />}
       {title}
     </a>
   );
--- src/components/LandingPage/Sale.jsx
+++ src/components/LandingPage/Sale.jsx
@@ -18,19 +18,19 @@ export const Sale = () => {
         <div className="flex px-6 flex-col gap-6 md:gap-8 justify-center items-center text-center max-w-2xl mx-auto mb-6 md:mb-8">
           <motion.h2
             className="text-3xl md:text-5xl font-extrabold "
-            // initial={{ opacity: 0, y: 30 }}
-            // whileInView={{ opacity: 1, y: 0 }}
-            // transition={{ duration: 1.2, delay: 0.2 }}
-            // viewport={{ once: true }}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.2, delay: 0.2 }}
+            viewport={{ once: true }}
           >
             Comece a se mover para ir ainda mais longe.
           </motion.h2>
           <motion.p
             className="text-lg md:text-xl text-brand-gray-800"
-            // initial={{ opacity: 0, y: 30 }}
-            // whileInView={{ opacity: 1, y: 0 }}
-            // transition={{ duration: 1.2, delay: 0.5 }}
-            // viewport={{ once: true }}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.2, delay: 0.5 }}
+            viewport={{ once: true }}
           >
             Chegou a hora de cuidar de você e da sua saúde. Tenha acesso a
             diversas modalidades de treinos divertidos, Calendário, Maratonas,
@@ -48,7 +48,8 @@ export const Sale = () => {
           className=" max-w-[1080px] h-auto px-6 xl:px-0  flex flex-col md:grid grid-cols-3 lg:gap-12      mx-auto gap-6     "
         >
           {sale.map((item, index) => (
-            <CardSale key={index} cards={item} delay={index} inView={inView} />
+            <CardSale key={index} cards={item} delay={index}  inView={inView}
+            />
           ))}
         </div>
         <div className="container-margin pt-12 md:pt-16 md:grid grid-cols-2 gap-6 md:gap-8 px-6 xl:px-0">
@@ -70,28 +71,25 @@ export const Sale = () => {
                 alt="Selo de Garantia"
                 className="w-[30%]"
                 loading="lazy"
-                // initial={{ opacity: 0, scale: 0 }}
-                // animate={{ opacity: 1, scale: 1 }}
-                // transition={{
-                //   duration: 0.4,
-                //   scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-                // }}
-                // viewport={{ once: true }}
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.4,
+                  scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                }}
+                viewport={{ once: true }}
               />
             </div>
-            <motion.p
-              className="text-lg md:text-xl text-brand-gray-800"
-              // initial={{ opacity: 0, y: 50 }}
-              // whileInView={{ opacity: 1, y: 0 }}
-              // transition={{ duration: 1 }}
-              // viewport={{ once: true }}
-            >
+            <motion.p className="text-lg md:text-xl text-brand-gray-800"  initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}>
               <strong className="font-bold text-brand-dark mr-1">
                 {" "}
                 Ao assinar um dos nossos planos, você tem 15 dias para
-                experimentar tudo que oferecemos.
+                experimentar tudo que oferecemos. 
               </strong>
-              A chance dos treinos da Solta Move não mexerem com você é muito
+               A chance dos treinos da Solta Move não mexerem com você é muito
               pequena. Mas ninguém é perfeita, nem mesmo a Solta Move. Se você
               não se sentir mais viva, confiante e saudável, basta cancelar sua
               assinatura dentro deste período da garantia. Reembolsamos 100% do
@@ -102,50 +100,44 @@ export const Sale = () => {
             className="drop-shadow-xl"
             src={imagem}
             alt="Todos os serviços que podem ser adiquiridos "
-            // initial={{ opacity: 0, y: 30 }}
-            // whileInView={{ opacity: 1, y: 0 }}
-            // transition={{ duration: 1, delay: 0.5 }}
-            // viewport={{ once: true }}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            viewport={{ once: true }}
           />
         </div>
       </div>
       <div className="container-margin pt-12 md:pt-16 flex items-center flex-col gap-6  px-6 xl:px-0">
-        <motion.h2
-          className="text-3xl md:text-5xl font-extrabold text-center "
-          // initial={{ opacity: 0, y: 30 }}
-          // whileInView={{ opacity: 1, y: 0 }}
-          // transition={{ duration: 1 }}
-          // viewport={{ once: true }}
-        >
+        <motion.h2 className="text-3xl md:text-5xl font-extrabold text-center " initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}>
           Treine no seu dispositivo favorito.
         </motion.h2>
         <motion.img
           src={imagemDispositivoDesktop}
           alt="Plataforma solta em diversos dispositivos"
           className="w-[80%] lg:w-[90%] hidden md:block"
-          // initial={{ opacity: 0, y: 50 }}
-          // whileInView={{ opacity: 1, y: 0 }}
-          // transition={{ duration: 1 }}
-          // viewport={{ once: true }}
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
         />
         <motion.img
           src={imagemDispositivoMobile}
           alt="Plataforma solta em diversos dispositivos"
           className="w-full block md:hidden"
-          // initial={{ opacity: 0, y: 50 }}
-          // whileInView={{ opacity: 1, y: 0 }}
-          // transition={{ duration: 1 }}
-          // viewport={{ once: true }}
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
         />
       </div>
       <div className=" py-12 md:py-16 px-6 xl:px-0 max-w-2xl mx-auto flex flex-col items-center gap-2 hidden">
-        <motion.div
-          className="mx-auto bg-brand-gray-100  rounded-2xl md:rounded-3xl p-8 md:p-12  gap-8 flex flex-col"
-          // initial={{ opacity: 0, y: 30 }}
-          // whileInView={{ opacity: 1, y: 0 }}
-          // transition={{ duration: 1.2, delay: 0.2 }}
-          // viewport={{ once: true }}
-        >
+        <motion.div className="mx-auto bg-brand-gray-100  rounded-2xl md:rounded-3xl p-8 md:p-12  gap-8 flex flex-col" initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.2 }}
+          viewport={{ once: true }}>
           <h3 className="text-3xl md:text-5xl font-extrabold text-center ">
             Uma Move a Outra.
           </h3>
@@ -159,13 +151,10 @@ export const Sale = () => {
             </strong>
           </p>
         </motion.div>
-        <motion.p
-          className="text-sm text-brand-gray-700 mx-auto"
-          // initial={{ opacity: 0, y: 50 }}
-          // whileInView={{ opacity: 1, y: 0 }}
-          // transition={{ duration: 1.2, delay: 0.5 }}
-          // viewport={{ once: true }}
-        >
+        <motion.p className="text-sm text-brand-gray-700 mx-auto" initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.5 }}
+          viewport={{ once: true }}>
           *Fonte: Datafolha Instituto de Pesquisas
         </motion.p>
       </div>
